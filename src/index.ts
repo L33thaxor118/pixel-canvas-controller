d39428056c7a0a2fc3e881db7540c4f3ddab8567
Azriel Alvarado <alvarado_118@yahoo.com>
2022-05-28 12:15:57 -0500

    Simplify code

--- src/index.ts
+++ src/index.ts
@@ -1,46 +1,6 @@
 import { SerialPort } from 'serialport'
 import LedClient from 'azledclientnode'
 
-class SyncManager {
-  private communicator: MatrixCommunicator
-  constructor(communicator: MatrixCommunicator) {
-    this.communicator = communicator
-  }
-  private currMatrixState = new Array(256).fill(0)
-
-  enqueueSync(serverState: Array<number>) {
-    //deal with enqueueing later. for now just test
-    this.sync([...this.currMatrixState], serverState)
-  }
-
-  //this might take several seconds
-  async sync(matrixState: Array<number>, serverState: Array<number>) {
-    const commands = this.diff(matrixState, serverState)
-    for (const command of commands) {
-      await this.communicator.sendCommand(command)
-    }
-    this.currMatrixState = matrixState
-  }
-
-  private diff(matrixState: Array<number>, serverState: Array<number>): Array<PaintCommand> {
-    const commands = []
-    for (let i = 0; i < 256; i++) {
-      if (matrixState[i] != serverState[i]) {
-        const rgb = this.decimalToRgb(serverState[i])
-        commands.push({pos: i, r: rgb.r, g: rgb.g, b: rgb.b})
-      }
-    }
-    return commands
-  }
-
-  private decimalToRgb(color: number): {r: number, g: number, b: number} {
-    const r = Math.floor(color / (256*256));
-    const g = Math.floor(color / 256) % 256;
-    const b = color % 256;
-    return {r:r, g:g, b:b}
-  }
-}
-
 interface PaintCommand {
   pos: number,
   r: number,
@@ -48,40 +8,17 @@ interface PaintCommand {
   b: number
 }
 
-class MatrixCommunicator {
-  private port: SerialPort
-  constructor(port: SerialPort) {
-    this.port = port
-  }
-  public async sendCommand(command: PaintCommand): Promise<void> {
-    return new Promise<void>((resolve, reject)=>{
-      this.port.write(`[${command.pos},${command.r},${command.g},${command.b}]`, (error)=> {
-        if (error) {
-          reject()
-        } else {
-          this.port.on('data', (data)=>{
-            if (data == 'd') {
-              resolve()
-            } else {
-              reject()
-            }
-          })
-        }
-      })
-    })
-  }
-}
+const port = new SerialPort({
+  path: '/dev/tty.usbmodem11301',
+  baudRate: 9600
+})
 
-const manager = new SyncManager(
-  new MatrixCommunicator(
-    new SerialPort({
-      path: '/dev/tty.usbmodem11301',
-      baudRate: 9600
-    })
-  )
-)
+const currMatrixState: Array<number> = new Array(256).fill(0)
+const message = 0;
 
-LedClient.setStateListener((state)=>{
+LedClient.setStateListener( async (state)=>{
+  const mnum = message + 1;
+  console.log("received message " + mnum)
   //need to reorder matrix to fit LED matrix ordering
   const newMatrix: Array<Array<number>> = []
   state.forEach((row, idx) => {
@@ -91,5 +28,52 @@ LedClient.setStateListener((state)=>{
       newMatrix.push(row)
     }
   })
-  manager.enqueueSync(newMatrix.flat().reverse())
+  await sync([...currMatrixState], newMatrix.flat().reverse())
+  console.log("finished processing message " + mnum)
 })
+
+//this might take several seconds. If we are syncing
+//while a new state comes in, we can ignore all states except the very latest one. 
+async function sync(matrixState: Array<number>, serverState: Array<number>) {
+  const commands = diff(matrixState, serverState)
+  for (const command of commands) {
+    await sendCommand(port, command)
+  }
+  matrixState = serverState
+}
+
+async function sendCommand(port: SerialPort, command: PaintCommand): Promise<void> {
+  return new Promise<void>((resolve, reject)=>{
+    port.write(`[${command.pos},${command.r},${command.g},${command.b}]`, (error)=> {
+      if (error) {
+        reject()
+      } else {
+        port.on('data', (data)=>{
+          if (data == 'd') {
+            resolve()
+          } else {
+            reject()
+          }
+        })
+      }
+    })
+  })
+}
+
+function diff(matrixState: Array<number>, serverState: Array<number>): Array<PaintCommand> {
+  const commands = []
+  for (let i = 0; i < 256; i++) {
+    if (matrixState[i] != serverState[i]) {
+      const rgb = decimalToRgb(serverState[i])
+      commands.push({pos: i, r: rgb.r, g: rgb.g, b: rgb.b})
+    }
+  }
+  return commands
+}
+
+function decimalToRgb(color: number): {r: number, g: number, b: number} {
+  const r = Math.floor(color / (256*256));
+  const g = Math.floor(color / 256) % 256;
+  const b = color % 256;
+  return {r:r, g:g, b:b}
+}
